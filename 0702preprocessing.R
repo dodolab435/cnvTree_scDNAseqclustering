a51bcc7ca76e43eda8575912067c7a8c8e1569e0
dodolab435 <dodo435@gmail.com>
2025-08-06 03:56:45 +0000

    Initial commit

--- 0702preprocessing.R
+++ 0702preprocessing.R
@@ -24,15 +24,33 @@ PreprocessingFile <- function(input_file) {
   
   ################################################################################
   
-  if (startsWith(as.character(noFactorInput$seqnames[1]), "chr")) {
+  if (startsWith(as.character(noFactorInput$seqnames[1]), "chr")||
+    grepl("^chr", as.character(noFactorInput$seqnames[1]), ignore.case = TRUE)){
+    
+    vec <- unique(noFactorInput$seqnames)
+    chr_num <- vec[grepl("^chr\\d+$", vec, ignore.case = TRUE)]
+    nums <- as.numeric(gsub("(?i)^chr", "", chr_num)) #移除chr -> 換數字
+    
+    max_num <- max(nums, na.rm = TRUE)
+    main_chr <- paste0("chr", 1:max_num)
+    extra_chr <- setdiff(vec, main_chr) #抓非數字chr
+    
     noFactorInput$seqnames <- factor(
       noFactorInput$seqnames,
-      levels = c(paste0("chr", 1:22), "chrX", "chrY")
+      levels = c(main_chr, sort(extra_chr))
     )
   } else {
+    
+    vec <- unique(noFactorInput$seqnames)
+    
+    nums <- as.numeric(vec[grepl("^\\d+$", vec)])  # 純數字的
+    max_num <- max(nums, na.rm = TRUE)
+    main_chr <- as.character(1:max_num)
+    extra_chr <- setdiff(vec, main_chr)
+    
     noFactorInput$seqnames <- factor(
       noFactorInput$seqnames,
-      levels = c(1:22, "X", "Y")
+      levels = c(main_chr, sort(extra_chr))
     )
   }
   
@@ -64,12 +82,13 @@ Sample <- changeFormat(file = file_path, cores = 5)
 
 pqArm_result <- pqArmClustering(input = Sample, pqArm_file = "hg19")
 Consolidation_result <- clusterConsolidation(input = Sample, pqArm_output = pqArm_result, 
-                                             pqArm_file = "hg19") #, difratio_chr = 0.5
+                                             pqArm_file = "hg19", difratio_chr = 0.5) #
 
 Subclone_output <- SubClustering(input = Sample, Consolidating_output = Consolidation_result,
-                                 min_cell = 5) #, dif_ratio = 0.5, overlap_region = 10^8
+                                 min_cell = 5, dif_ratio = 0.5, overlap_region = 10^6) #
 
-scDNA_Output(input = Sample, Summary = Subclone_output, pqArm_file = "hg19", cellcutoff = 5) #, consecutive_region = 10^4
+scDNA_Output(input = Sample, Summary = Subclone_output, pqArm_file = "hg19", cellcutoff = 5, 
+             sexchromosome_plot=TRUE, consecutive_region = 10^4) #
 
 
 
